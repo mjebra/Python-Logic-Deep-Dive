9f0c443d8a645e583f0603e04d3cd9dc3fccd546
Mohammad Javad Ebrahimi <99821752+mjebra@users.noreply.github.com>
2026-04-05 17:08:45 +0330

    Create program.py
    This file explains the meaning of Program concept.

---
+++ program.py
@@ -0,0 +1,13 @@
+"""
+Concept: What is Program?
+Difinition: A sequence of instructions that process data to achive  goal.
+"""
+
+user_name = input("Hello! what is your name?")
+
+#this Function prints the welcome message te user_name
+def welcome_message(name):
+    print(f"Welcome {name} 🖤")
+
+#executes Function.
+welcome(user_name)
